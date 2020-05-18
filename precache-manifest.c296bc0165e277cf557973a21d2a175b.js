622f796bf851641e79049defa8daaca7653f1ea1
Ted Irland <58044717+tedirland@users.noreply.github.com>
2020-05-18 14:19:40 -0500

    Add files via upload

---
+++ precache-manifest.c296bc0165e277cf557973a21d2a175b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3104d8999c7ca1c806b4c6c3d7fe56a4",
+    "url": "/education/web/983256e6243a9cee47b4.worker.js"
+  },
+  {
+    "revision": "c16a1fb99a922d4750a8a245ca8977e6",
+    "url": "/education/web/index.html"
+  },
+  {
+    "revision": "e57badbff83e4deb6ed1",
+    "url": "/education/web/static/css/2.5b79b3b6.chunk.css"
+  },
+  {
+    "revision": "021db7a03e57e583e8dd",
+    "url": "/education/web/static/css/main.b74e9f84.chunk.css"
+  },
+  {
+    "revision": "e57badbff83e4deb6ed1",
+    "url": "/education/web/static/js/2.3f8e24b0.chunk.js"
+  },
+  {
+    "revision": "021db7a03e57e583e8dd",
+    "url": "/education/web/static/js/main.646e0a83.chunk.js"
+  },
+  {
+    "revision": "c74c6120b4dea440f0dc",
+    "url": "/education/web/static/js/runtime-main.0e7cf24a.js"
+  },
+  {
+    "revision": "2a72994b8a5a34fcb92ba89ffd862c4b",
+    "url": "/education/web/static/media/cover-cn.2a72994b.png"
+  },
+  {
+    "revision": "a2d244ac024c8c7f67a3a849597a5ba8",
+    "url": "/education/web/static/media/cover-us.a2d244ac.png"
+  },
+  {
+    "revision": "45b9371b21ff0d4e1ce607bc647ba267",
+    "url": "/education/web/static/media/pic-message@2x.45b9371b.png"
+  },
+  {
+    "revision": "f5d3055bb39ea0d98e47435a207b7040",
+    "url": "/education/web/static/media/pic-student.f5d3055b.png"
+  },
+  {
+    "revision": "580ff5f1a224a4b75cf57361e7f0d661",
+    "url": "/education/web/static/media/pic-teacher.580ff5f1.png"
+  }
+]);
